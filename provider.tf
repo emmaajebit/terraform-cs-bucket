bf00dec03807ea67989f241b6278af5eeeb05058
Sami Islam <islam.samipkr@gmail.com>
2023-03-18 12:56:08 -0400

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.57.0"
+    }
+  }
+}
+
+provider "google" {
+  project     = "avian-amulet-378416"
+  region      = "us-central1"
+}
+
+
+terraform {
+  cloud {
+    organization = "Teenops"
+
+    workspaces {
+      name = "TeenopsBoss"
+    }
+  }
+}
