6000fa7efce286bd63b275c659e71d19450a5694
Sami Islam <islam.samipkr@gmail.com>
2023-03-18 12:59:25 -0400

    Create var.tf

---
+++ var.tf
@@ -0,0 +1,18 @@
+variable "project_id" {
+  description = "The ID of the GCP project."
+  default     = "avian-amulet-378416"
+}
+
+variable "bucket_name" {
+  description = "A list of unique names for the GCP storage buckets."
+  type        = list(string)
+  default     = [
+    "my-test-bucket-latest8",
+    "mytestbucket88"
+
+  ]
+}
+variable "bucket_location" {
+  description = "The location of the GCP storage buckets."
+  default     = "US"
+}
